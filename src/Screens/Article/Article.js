7510e43837f2637c0148ca0207d373c017f535f0
Sumeshc99 <sumeshchaure99@gmail.com>
2023-05-17 09:31:29 +0530

    done article section

--- src/Screens/Article/Article.js
+++ src/Screens/Article/Article.js
@@ -7,7 +7,7 @@ function Article() {
   const [inputValue1, setInputValue1] = useState("");
   const [inputValue2, setInputValue2] = useState("");
 
-  console.log("aaaaaaaa",inputArray);
+  console.log("aaaaaaaa", inputArray);
 
   const handleInput1Change = (event) => {
     setInputValue1(event.target.value);
@@ -25,11 +25,41 @@ function Article() {
   };
 
   return (
-    <div className="ArticlePage">
-      <div className="col-12 col-lg-6 userinput">
-        <input className="name" type="text" value={inputValue1} onChange={handleInput1Change} placeholder="Enter Name"/>
-        <input className="article" type="text" value={inputValue2} onChange={handleInput2Change} placeholder="write our thoughts"/>
-        <button onClick={handleButtonClick}>Add to Array</button>
+    <div className="row m-1 ArticlePage">
+      <div className="col-12 col-lg-5  userinput">
+        <h2 className="ArticleTitle">
+          Share your Thoughts, knowledge and experience about Robotics
+        </h2>
+        <input
+          className="name"
+          type="text"
+          value={inputValue1}
+          onChange={handleInput1Change}
+          placeholder="Enter Name"
+        />
+        <textarea
+          className="article"
+          rows={4}
+          cols={50}
+          type="text"
+          value={inputValue2}
+          onChange={handleInput2Change}
+          placeholder="write our thoughts"
+        />
+        <button onClick={handleButtonClick}>Send</button>
+      </div>
+      <div className="col-12 col-lg-7  userinput">
+        <h1 className="explore">Explore Shared information </h1>
+        <h1 className="explore">by the peaple</h1>
+
+        <div>
+          {inputArray.map((obj, index) => (
+            <p newObj={index}>{obj.input1} </p>
+          ))}
+          {inputArray.map((obj, index) => (
+            <p newObj={index}>{obj.input2} </p>
+          ))}
+        </div>
       </div>
     </div>
   );
