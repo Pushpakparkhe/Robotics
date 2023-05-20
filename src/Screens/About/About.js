8c1d68149cdfc426b0075be6c5fb244e6980eea6
Pushpakparkhe <pushpakparkhe2@gmail.com>
2023-05-20 14:06:18 +0530

    first commit

--- src/Screens/About/About.js
+++ src/Screens/About/About.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+   <section className="section-white">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <h2 className="section-title">
+            the team behind robotics
+          </h2>
+        </div>
+      </div>
+    </div>
+
+   </section>
   )
 }
 
